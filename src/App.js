6c6eaaa499f2289ff8c0d32c35da80e681acd6bb
AstroTetsuwan <tetsuwan.astroboy@gmail.com>
2019-04-10 13:03:10 +0200

    removed things not related to the background

--- src/App.js
+++ src/App.js
@@ -3,14 +3,12 @@ import React from 'react';
 import './App.css';
 
 import SkyBackground from './SkyBackground/SkyBackground';
-import Portfolio from './Portfolio/Portfolio';
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
         <SkyBackground/>
-        <Portfolio/>
       </div>
     );
   }
